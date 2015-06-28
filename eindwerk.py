0fb40faeade99bee6593ffb156da8ff3dae47316
Daniël De Koker <dandek01@gmail.com>
2015-06-28 20:55:14 +0200

    chapter1

--- eindwerk.py
+++ eindwerk.py
@@ -1,12 +1,3 @@
-from flask import Flask
+from views import app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+app.run(debug=True)
